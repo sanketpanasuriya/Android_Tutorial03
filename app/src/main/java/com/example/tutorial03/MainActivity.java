6b341a0f87cae77a8f112a9c86290d4ebd48e20e
sanketpanasuriya <58542407+sanketpanasuriya@users.noreply.github.com>
2020-07-29 11:06:44 +0530

    Main Activity Change Commit

--- app/src/main/java/com/example/tutorial03/MainActivity.java
+++ app/src/main/java/com/example/tutorial03/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkDataEntered();
-
             }
         });
     }
